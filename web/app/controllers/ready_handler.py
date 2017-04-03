3c512e0297f5f4321fbb7c0c4787cae4d782de7f
Samuel Sekiwere <sekiskylink@gmail.com>
2017-04-03 16:19:49 +0300

    fix ready requests page

--- web/app/controllers/ready_handler.py
+++ web/app/controllers/ready_handler.py
@@ -29,3 +29,33 @@ class Ready:
         l = locals()
         del l['self']
         return render.ready(**l)
+
+    @require_login
+    def POST(self):
+        params = web.input(page=1, reqid=[])
+        try:
+            page = int(params.page)
+        except:
+            page = 1
+        limit = PAGE_LIMIT
+        start = (page - 1) * limit if page > 0 else 0
+
+        with db.transaction():
+            if params.abtn == 'Cancel Selected':
+                if params.reqid:
+                    for val in params.reqid:
+                        db.update('requests', where="id = %s" % val, status='canceled')
+
+            db.transaction().commit()
+        dic = lit(
+            relations='requests', fields="*",
+            criteria="status='ready'",
+            order="id desc",
+            limit=limit, offset=start)
+        res = doquery(db, dic)
+        count = countquery(db, dic)
+        pagination_str = getPaginationString(default(page, 0), count, limit, 2, "ready", "?page=")
+
+        l = locals()
+        del l['self']
+        return render.ready(**l)
