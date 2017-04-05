68b57826b78d8138a6317a472ed570da4e741a9a
Samuel Sekiwere <sekiskylink@gmail.com>
2017-04-06 01:18:08 +0300

    Modifications on reporters page

--- web/app/controllers/reporters_handler.py
+++ web/app/controllers/reporters_handler.py
@@ -1,7 +1,9 @@
 import web
-from . import csrf_protected, db, require_login, get_session, render
-from app.tools.utils import audit_log
 import json
+from . import csrf_protected, db, require_login, get_session, render, allDistrictsByName
+from app.tools.utils import audit_log, default, lit
+from app.tools.pagination2 import doquery, countquery, getPaginationString
+from settings import PAGE_LIMIT
 
 
 class Reporters:
@@ -21,18 +23,26 @@ class Reporters:
                 "(SELECT id FROM locationtype WHERE name = 'district') ORDER by name")
 
         districts = db.query(districts_SQL)
-        roles = db.query("SELECT id, name from reporter_groups order by name")
         district = {}
+        roles = db.query("SELECT id, name from reporter_groups order by name")
         allow_edit = False
+
         try:
             edit_val = int(params.ed)
             allow_edit = True
         except ValueError:
             pass
+        try:
+            page = int(params.page)
+        except:
+            page = 1
+        limit = PAGE_LIMIT
+        start = (page - 1) * limit if page > 0 else 0
+
         if params.ed and allow_edit:
             res = db.query(
-                "SELECT id, firstname, lastname, telephone, email, national_id, "
-                "reporting_location, distribution_point, role, dpoint,alternate_tel "
+                "SELECT id, firstname, lastname, telephone, email, "
+                "reporting_location, role, alternate_tel, facilityid, facility "
                 "FROM reporters_view "
                 " WHERE id = $id", {'id': edit_val})
             if res:
@@ -42,36 +52,31 @@ class Reporters:
                 telephone = r.telephone
                 email = r.email
                 role = r.role
-                national_id = r.national_id
                 alt_telephone = r.alternate_tel
-                dpoint_id = r.distribution_point
-                dpoint = r.dpoint
                 location = r.reporting_location
                 subcounty = ""
+                facilityid = r.facilityid
                 district = ""
-                parish = ""
-                village = location
-                villages = []
-                parishes = []
                 subcounties = []
                 ancestors = db.query(
                     "SELECT id, name, level FROM get_ancestors($loc) "
                     "WHERE level > 1 ORDER BY level DESC;", {'loc': location})
                 if ancestors:
                     for loc in ancestors:
-                        if loc['level'] == 5:
-                            village = loc
-                        elif loc['level'] == 4:
-                            parish = loc
-                            villages = db.query("SELECT id, name FROM get_children($id)", {'id': loc['id']})
-                        elif loc['level'] == 3:
+                        if loc['level'] == 3:
                             subcounty = loc
-                            parishes = db.query("SELECT id, name FROM get_children($id)", {'id': loc['id']})
                         elif loc['level'] == 2:
                             district = loc
                             subcounties = db.query("SELECT id, name FROM get_children($id)", {'id': loc['id']})
                 else:
                     district = location
+                facilities = db.query(
+                    "SELECT id, name FROM healthfacilities WHERE location=$loc", {'loc': location})
+                if facilityid:
+                    fres = db.query(
+                        "SELECT id, name FROM healthfacilities WHERE id = $id", {'id': facilityid})
+                    facility = fres[0]
+
         allow_del = False
         try:
             del_val = int(params.d_id)
@@ -79,7 +84,7 @@ class Reporters:
         except ValueError:
             pass
         if params.d_id and allow_del:
-            if session.role in ('Micro Planning', 'Administrator'):
+            if session.role in ('District User', 'Administrator'):
                 reporter = db.query(
                     "SELECT firstname || ' ' || lastname as name , telephone "
                     "FROM reporters WHERE id = $id", {'id': params.d_id})
@@ -92,27 +97,66 @@ class Reporters:
                         'user': session.sesid
                     }
                     db.query("DELETE FROM reporter_groups_reporters WHERE reporter_id=$id", {'id': params.d_id})
+                    db.query("DELETE FROM reporter_healthfacility WHERE reporter_id=$id", {'id': params.d_id})
                     db.query("DELETE FROM reporters WHERE id=$id", {'id': params.d_id})
                     audit_log(db, log_dict)
                     if params.caller == "api":  # return json if API call
                         web.header("Content-Type", "application/json; charset=utf-8")
                         return json.dumps({'message': "success"})
-        if session.role == 'Administrator':
-            if params.search:
-                REPORTER_SQL = (
-                    "SELECT * FROM reporters_view4 WHERE  telephone ilike $search OR "
-                    " firstname ilike $search OR lastname ilike $search")
-            else:
-                REPORTER_SQL = "SELECT * FROM reporters_view4 WHERE id >  (SELECT max(id) - 8 FROM reporters);"
-        else:
-            if params.search:
-                REPORTER_SQL = (
-                    "SELECT * FROM reporters_view4 WHERE created_by = $user AND "
-                    " (firstname ilike $search OR lastname ilike $search OR telephone ilike $search)")
-            else:
-                REPORTER_SQL = "SELECT * FROM reporters_view4 WHERE created_by = $user ORDER BY id DESC LIMIT 50"
 
-        reporters = db.query(REPORTER_SQL, {'user': session.sesid, 'search': '%%%s%%' % params.search})
+        if session.role == 'District User':
+            district_id = allDistrictsByName['%s' % session.username.capitalize()]
+            criteria = "district_id=%s" % district_id
+            if params.search:
+                criteria += (
+                    " AND (telephone ilike '%%%s%%' OR "
+                    "firstname ilike '%%%s%%' OR lastname ilike '%%%s%%')")
+                criteria = criteria % (params.search, params.search, params.search)
+                dic = lit(
+                    relations='reporters_view',
+                    fields=(
+                        "id, firstname, lastname, telephone, district_id, "
+                        "facility, role, total_reports, last_reporting_date "),
+                    criteria=criteria,
+                    order="facility, firstname, lastname",
+                    limit=limit, offset=start)
+            else:
+                dic = lit(
+                    relations='reporters_view',
+                    fields=(
+                        "id, firstname, lastname, telephone, district_id, "
+                        "facility, role, total_reports, last_reporting_date "),
+                    criteria=criteria,
+                    order="",
+                    limit=limit, offset=start)
+        else:
+            criteria = ""
+            if params.search:
+                criteria += (
+                    " AND (telephone ilike '%s' OR "
+                    "firstname ilike '%s' OR lastname ilike '%s')")
+                dic = lit(
+                    relations='reporters_view',
+                    fields=(
+                        "id, firstname, lastname, telephone, district_id, "
+                        "facility, role, total_reports, last_reporting_date "),
+                    criteria=criteria,
+                    order="facility, firstname, lastname",
+                    limit=limit, offset=start)
+            else:
+                criteria = "id >  (SELECT max(id) - 250 FROM reporters)"
+                dic = lit(
+                    relations='reporters_view',
+                    fields=(
+                        "id, firstname, lastname, telephone, district_id, "
+                        "facility, role, total_reports, last_reporting_date "),
+                    criteria=criteria,
+                    order="facility, firstname, lastname",
+                    limit=limit, offset=start)
+
+        reporters = doquery(db, dic)
+        count = countquery(db, dic)
+        pagination_str = getPaginationString(default(page, 0), count, limit, 2, "reporters", "?page=")
         l = locals()
         del l['self']
         return render.reporters(**l)
@@ -122,8 +166,8 @@ class Reporters:
     def POST(self):
         session = get_session()
         params = web.input(
-            firstname="", lastname="", telephone="", email="", location="", dpoint="",
-            national_id="", role="", alt_telephone="", page="1", ed="", d_id="", district="")
+            firstname="", lastname="", telephone="", email="", location="",
+            role=[], alt_telephone="", page="1", ed="", d_id="", district="")
 
         allow_edit = False
         try:
@@ -135,17 +179,15 @@ class Reporters:
         with db.transaction():
             if params.ed and allow_edit:
                 location = params.location if params.location else None
-                dpoint = params.dpoint if params.dpoint else None
                 r = db.query(
                     "UPDATE reporters SET firstname=$firstname, lastname=$lastname, "
                     "telephone=$telephone, email=$email, reporting_location=$location, "
-                    "distribution_point=$dpoint, national_id=$nid, alternate_tel=$alt_tel, "
-                    "district_id = $district_id "
+                    "alternate_tel=$alt_tel, district_id = $district_id "
                     "WHERE id=$id RETURNING id", {
                         'firstname': params.firstname, 'lastname': params.lastname,
                         'telephone': params.telephone, 'email': params.email,
-                        'location': location, 'dpoint': dpoint, 'nid': params.national_id,
-                        'id': params.ed, 'alt_tel': params.alt_telephone, 'district_id': params.district
+                        'location': location, 'id': params.ed, 'alt_tel': params.alt_telephone,
+                        'district_id': params.district
                     })
                 if r:
                     db.query(
@@ -162,7 +204,6 @@ class Reporters:
                 return web.seeother("/reporters")
             else:
                 location = params.location if params.location else None
-                dpoint = params.dpoint if params.dpoint else None
                 has_reporter = db.query(
                     "SELECT id FROM reporters WHERE telephone = $tel", {'tel': params.telephone})
                 if has_reporter:
@@ -173,14 +214,14 @@ class Reporters:
                 session.rdata_err = ""
                 r = db.query(
                     "INSERT INTO reporters (firstname, lastname, telephone, email, "
-                    " reporting_location, distribution_point, national_id, alternate_tel, uuid, "
-                    " created_by, district_id) VALUES "
-                    " ($firstname, $lastname, $telephone, $email, $location, $dpoint,"
-                    " $nid, $alt_tel, uuid_generate_v4(), $user, $district_id) RETURNING id", {
+                    " reporting_location, alternate_tel, "
+                    " district_id) VALUES "
+                    " ($firstname, $lastname, $telephone, $email, $location, "
+                    " $alt_tel, $district_id) RETURNING id", {
                         'firstname': params.firstname, 'lastname': params.lastname,
                         'telephone': params.telephone, 'email': params.email,
-                        'location': location, 'dpoint': dpoint, 'nid': params.national_id,
-                        'alt_tel': params.alt_telephone, 'user': session.sesid, 'district_id': params.district
+                        'location': location, 'alt_tel': params.alt_telephone,
+                        'district_id': params.district
                     })
                 if r:
                     reporter_id = r[0]['id']
