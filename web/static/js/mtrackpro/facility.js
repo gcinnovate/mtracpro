85106210f1795fd5ed98d37a12d419bd4974f769
Samuel Sekiwere <sekiskylink@gmail.com>
2017-05-06 08:55:45 +0300

    changes on facilities dashboard

--- web/static/js/mtrackpro/facility.js
+++ web/static/js/mtrackpro/facility.js
@@ -8,4 +8,17 @@ $(function(){
                 $('#modal_res4').html(data);
             });
     });
+	$('#sendsms').click(function(){
+        $('#modal_res2').css({'color': 'green'});
+        facilityid = $('#facilityid').val()
+        sms = $('#sms').val()
+        role = $('#role').val()
+        $.post(
+            '/api/v1/facilitysms/' + facilityid,
+            {sms:sms, role:role},
+            function(data){
+            $('#modal_res2').html(data);
+        });
+        return false;
+    });
 });
