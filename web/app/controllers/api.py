f92ef16b52ce87b9ad98055e2895b9d0246ae2cb
Samuel Sekiwere <sekiskylink@gmail.com>
2017-10-05 14:06:58 +0300

    introduce a hack to handle special case of the cases/death reports when zeros are reported

--- web/app/controllers/api.py
+++ web/app/controllers/api.py
@@ -267,6 +267,9 @@ class Dhis2Queue:
                 label = v.get('label')
                 if val.__str__().isdigit():
                     slug = "%s_%s" % (params.form, label)
+                    if not(val) and params.form in ['cases', 'death']:
+                        if label not in params.raw_msg.lower():
+                            continue  # skip zero values for cases and death
                     print "%s=>%s" % (slug, val), MAPPING[slug]
                     if PREFERED_DHIS2_CONTENT_TYPE == 'json':
                         dataValues.append(
