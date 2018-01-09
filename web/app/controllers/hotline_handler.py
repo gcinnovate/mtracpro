a5768e07904c794d895a6f09606b9f1fd6ccf10b
Samuel Sekiwere <sekiskylink@gmail.com>
2018-01-09 12:48:58 +0300

    add anonymous hotline handler

---
+++ web/app/controllers/hotline_handler.py
@@ -0,0 +1,33 @@
+
+import web
+from . import csrf_protected, db, require_login, render
+
+
+class Hotline:
+    @require_login
+    def GET(self):
+        params = web.input(page=1, ed="", d_id="")
+        edit_val = params.ed
+        l = locals()
+        del l['self']
+        return render.hotline(**l)
+
+    @csrf_protected
+    @require_login
+    def POST(self):
+        params = web.input(
+            name="", descr="", page="1", ed="", d_id="")
+        try:
+            page = int(params.page)
+        except:
+            page = 1
+
+        with db.transaction():
+            if params.ed:
+                return web.seeother("/hotline")
+            else:
+                return web.seeother("/hotline")
+
+        l = locals()
+        del l['self']
+        return render.hotline(**l)
