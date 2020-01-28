9aa038353630f3c7d358f543d67b0645a381ccaf
Samuel Sekiwere <sekiskylink@gmail.com>
2020-01-28 14:03:12 +0300

    fix load_reporters script

--- web/load_reporters.py
+++ web/load_reporters.py
@@ -1,6 +1,7 @@
 import phonenumbers
 import getopt
 import sys
+import simplejson
 import psycopg2
 import psycopg2.extras
 from settings import config
@@ -34,7 +35,7 @@ with open(filename, 'r') as f:
     for l in f:
         reporter = l.strip().split("#")
         name, phone, district, facility, roles, is_active, facility_code = tuple(reporter)
-        print tuple(reporter)
+        # print tuple(reporter)
         names = name.split(' ', 1)
         if len(names) > 1:
             firstname = names[1]
@@ -42,32 +43,36 @@ with open(filename, 'r') as f:
         else:
             firstname = ''
             lastname = names[0]
+        if is_active == 'f':
+            continue
         cur.execute("SELECT id FROM reporters WHERE telephone='%s'" % phone)
         res = cur.fetchone()
         if res:  # we already have reporter
+            # XXX # XXX XXX
+            # continue
+            # XXX # XXX XXX
+            print("Updating Reporter:", phone)
+
             reporter_id = res['id']
             cur.execute(
                 "SELECT id, district_id, location, location_name "
                 "FROM healthfacilities WHERE code = %s", [facility_code])
             f = cur.fetchone()
             if f:
+                print("Falcility Exists", facility_code, f['district_id'])
                 cur.execute(
                     "UPDATE reporters SET (firstname, lastname, district_id, reporting_location, "
-                    " reporting_location_name) = (%s, %s, %s, %s, %s) WHERE id = %s",
-                    [firstname, lastname, f['district_id'], f['location'], f['location_name'], reporter_id])
-                cur.execute(
-                    "UPDATE reporter_healthfacility SET facility_id = %s "
-                    "WHERE reporter_id = %s", [f['id'], reporter_id])
-                cur.execute("DELETE FROM reporter_groups_reporters WHERE reporter_id = %s", [reporter_id])
-                reporter_roles = roles.split(',')
-                for role in reporter_roles:
-                    cur.execute(
-                        "INSERT INTO reporter_groups_reporters (reporter_id, group_id) "
-                        "VALUES (%s, (SELECT id FROM reporter_groups WHERE name=%s))",
-                        [reporter_id, role])
+                    " reporting_location_name, facilityid, groups, updated) = (%s, %s, %s, %s, %s, %s,"
+                    "(SELECT array_agg(id) FROM reporter_groups WHERE name IN "
+                    "(SELECT name FROM  regexp_split_to_table(%s, E',') as name)), NOW()) WHERE id = %s",
+                    [
+                        firstname, lastname, f['district_id'],
+                        f['location'], f['location_name'],
+                        f['id'], roles, reporter_id])
                 conn.commit()
 
         else:  # new reporter
+            print tuple(reporter)
             cur.execute(
                 "SELECT id, district_id, location, location_name "
                 "FROM healthfacilities WHERE code = %s", [facility_code])
@@ -75,56 +80,43 @@ with open(filename, 'r') as f:
             if f:
                 reporterSQL = (
                     "INSERT INTO reporters(firstname, lastname, telephone, district_id, "
-                    "reporting_location, reporting_location_name) "
-                    "VALUES(%s, %s, %s, %s, %s, %s) RETURNING id"
+                    "reporting_location, reporting_location_name, facilityid, groups, jparents) "
+                    "VALUES(%s, %s, %s, %s, %s, %s, %s, "
+                    "(SELECT array_agg(id) FROM reporter_groups WHERE name IN "
+                    "(SELECT name FROM  regexp_split_to_table(%s, E',') as name))::INT[], "
+                    "%s::json) RETURNING id"
                 )
+                ancestor_locations = psycopg2.extras.Json({
+                    'd': f['district_id'],
+                    's': f['location']}, dumps=simplejson.dumps)
                 cur.execute(reporterSQL, [
                     firstname, lastname, phone, f['district_id'],
-                    f['location'], f['location_name']])
+                    f['location'], f['location_name'], f['id'], roles, ancestor_locations])
                 conn.commit()
-                rs = cur.fetchone()
-                if rs:
-                    reporter_id = rs['id']
-                    cur.execute(
-                        "INSERT INTO reporter_healthfacility (reporter_id, facility_id) "
-                        "VALUES (%s, %s)", [reporter_id, f['id']])
-                    reporter_roles = roles.split(',')
-                    for role in reporter_roles:
-                        cur.execute(
-                            "INSERT INTO reporter_groups_reporters (reporter_id, group_id) "
-                            "VALUES (%s, (SELECT id FROM reporter_groups WHERE name=%s))",
-                            [reporter_id, role])
-                    conn.commit()
             else:
                 # try DHO's Office
-                if facility.__contains__('DHO'):
+                if facility.__contains__('DHO') or facility == district:
                     cur.execute(
                         "SELECT id, district_id FROM healthfacilities WHERE name = %s",
-                        [district + " DHO's Office"])
+                        [district + " DHO Office"])
                     dres = cur.fetchone()
                     if dres:
                         f = dres
                         reporterSQL = (
                             "INSERT INTO reporters(firstname, lastname, telephone, district_id, "
-                            "reporting_location, reporting_location_name) "
-                            "VALUES(%s, %s, %s, %s, %s, %s) RETURNING id"
+                            "reporting_location, reporting_location_name, facilityid, groups) "
+                            "VALUES(%s, %s, %s, %s, %s, %s, %s, "
+                            "(SELECT array_agg(id) FROM reporter_groups WHERE name IN "
+                            "(SELECT name FROM  regexp_split_to_table(%s, E',') as name))::INT[]) RETURNING id"
                         )
                         cur.execute(reporterSQL, [
                             firstname, lastname, phone, f['district_id'],
-                            f['location'], f['location_name']])
+                            f['location'], f['location_name'], f['id'], roles])
+
                         conn.commit()
                         rs = cur.fetchone()
-                        if rs:
-                            reporter_id = rs['id']
-                            cur.execute(
-                                "INSERT INTO reporter_healthfacility (reporter_id, facility_id) "
-                                "VALUES (%s, %s)", [reporter_id, f['id']])
-                            reporter_roles = roles.split(',')
-                            for role in reporter_roles:
-                                cur.execute(
-                                    "INSERT INTO reporter_groups_reporters (reporter_id, group_id) "
-                                    "VALUES (%s, (SELECT id FROM reporter_groups WHERE name=%s))",
-                                    [reporter_id, role])
-                            conn.commit()
-                    print district, phone, facility_code
+                    print(district, phone, facility_code)
+                else:
+                    # facilities not synchronized
+                    print("Other=>", l.strip())
 conn.close()
