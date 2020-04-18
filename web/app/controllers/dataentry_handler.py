c1978e4710b4ec83152557a768543d3cfcaaec87
Samuel Sekiwere <sekiskylink@gmail.com>
2020-04-18 11:38:49 +0300

    fix data entry page

--- web/app/controllers/dataentry_handler.py
+++ web/app/controllers/dataentry_handler.py
@@ -89,46 +89,46 @@ class DataEntry:
         msg += generate_raw_message(db, report, dataDict, addCommands)
         # print "================>", msg
 
-        if facilitycode and dataValues:
-            args_dict = {
-                'completeDate': datetime.datetime.now().strftime("%Y-%m-%d"),
-                'period': week,
-                'orgUnit': facilitycode,
-                'dataValues': dataValues
-            }
-            if PREFERED_DHIS2_CONTENT_TYPE == 'json':
-                args_dict['dataSet'] = HMIS_033B_DATASET
-                args_dict['attributeOptionCombo'] = HMIS_033B_DATASET_ATTR_OPT_COMBO
-                payload = json.dumps(args_dict)
-            else:
-                payload = XML_TEMPLATE % args_dict
-            year, _week = tuple(args_dict['period'].split('W'))
-            print(payload)
-            extra_params = {
-                'week': _week, 'year': year, 'msisdn': params.reporter,
-                'facility': facilitycode, 'raw_msg': msg,
-                'district': params.districtname, 'report_type': params.report,
-                'source': serversByName[config['dispatcher2_source']],
-                'destination': serversByName[config['dispatcher2_destination']],
-                'extras': json.dumps(extras),
-                'status': config.get('default-queue-status', 'pending'),
-                'body': payload}
-            # now ready to queue to DB for pushing to DHIS2
-            # resp = queue_submission(serverid, post_xml, year, week)
-            print(extra_params)
-            if PREFERED_DHIS2_CONTENT_TYPE == 'json':
-                extra_params['ctype'] = 'json'
-                # resp = post_request_to_dispatcher2(
-                #    payload, params=extra_params, ctype='application/json')
-                queue_request(db, extra_params)
-            else:
-                extra_params['ctype'] = 'xml'
-                queue_request(db, extra_params)
-                # resp = post_request_to_dispatcher2(payload, params=extra_params)
-            # print "Resp:", resp
-
         with db.transaction():
-            pass
+            if facilitycode and dataValues:
+                args_dict = {
+                    'completeDate': datetime.datetime.now().strftime("%Y-%m-%d"),
+                    'period': week,
+                    'orgUnit': facilitycode,
+                    'dataValues': dataValues
+                }
+                if PREFERED_DHIS2_CONTENT_TYPE == 'json':
+                    args_dict['dataSet'] = HMIS_033B_DATASET
+                    args_dict['attributeOptionCombo'] = HMIS_033B_DATASET_ATTR_OPT_COMBO
+                    payload = json.dumps(args_dict)
+                else:
+                    payload = XML_TEMPLATE % args_dict
+                year, _week = tuple(args_dict['period'].split('W'))
+                print(payload)
+                extra_params = {
+                    'week': _week, 'year': year, 'msisdn': params.reporter,
+                    'facility': facilitycode, 'raw_msg': msg,
+                    'district': params.districtname, 'report_type': params.report,
+                    'source': serversByName[config['dispatcher2_source']],
+                    'destination': serversByName[config['dispatcher2_destination']],
+                    'extras': json.dumps(extras),
+                    'status': config.get('default-queue-status', 'pending'),
+                    'body': payload}
+                # now ready to queue to DB for pushing to DHIS2
+                # resp = queue_submission(serverid, post_xml, year, week)
+                print(extra_params)
+                if PREFERED_DHIS2_CONTENT_TYPE == 'json':
+                    extra_params['ctype'] = 'json'
+                    # resp = post_request_to_dispatcher2(
+                    #    payload, params=extra_params, ctype='application/json')
+                    queue_request(db, extra_params)
+                else:
+                    extra_params['ctype'] = 'xml'
+                    queue_request(db, extra_params)
+                    # resp = post_request_to_dispatcher2(payload, params=extra_params)
+                # print "Resp:", resp
+                return web.seeother("/dataentry")
+
         l = locals()
         del l['self']
         return render.dataentry(**l)
