797bb34e9ab79094ed8de44166425265b878524c
Samuel Sekiwere <sekiskylink@gmail.com>
2024-04-13 09:25:08 +0300

    modify rapidpro reporter sync script

--- web/upload_rapidpro_reporters2.py
+++ web/upload_rapidpro_reporters2.py
@@ -19,7 +19,7 @@ opts, args = getopt.getopt(
     [])
 
 now = datetime.datetime.now()
-sdate = now - datetime.timedelta(days=0, minutes=30)
+sdate = now - datetime.timedelta(days=0, minutes=5)
 from_date = sdate.strftime('%Y-%m-%d %H:%M')
 update_date = from_date
 
@@ -83,7 +83,7 @@ def add_reporter_fields():
         if f['label'] not in fields:
             res = post_request(
                 json.dumps(f), '%sfields.json' % config['api_url'])
-            print res.text
+            print(res.text)
 
 if ADD_FIELDS:
     add_reporter_fields()
@@ -102,9 +102,11 @@ cur.execute(
 )
 
 res = cur.fetchall()
-# print "==>", res
+print(f"Records Found: {len(res)}")
+# sys.exit(1)
 if res:
     for r in res:
+        # print(f"processing: {r['telephone']}")
         district = r["district"]
         existing_uuid = r["uuid"]
         endpoint = config["default_api_uri"]
@@ -121,93 +123,102 @@ if res:
             fields["district"] = district
         phone = format_msisdn(r['telephone']) if r['telephone'] else ''
         alt_phone = format_msisdn(r['alternate_tel']) if r['alternate_tel'] else ''
-        if phone:
-            data = {
-                "name": r['name'],
-                "urns": ["tel:" + phone],
-                "email": r['email'],
-                "groups": r['role'].split(','),
-                "fields": fields
-            }
-            data2 = {
-                "name": r['name'],
-                "email": r['email'],
-                "groups": r['role'].split(','),
-                "fields": fields
-            }
-            # Let's try getting the contact
-            get_url = "{0}urn={1}".format(endpoint, "tel:" + phone)
-            # print(get_url)
-            resp = get_request(get_url)
-            json_resp = resp.json()
-            if json_resp['results']:
-                url = "{0}uuid={1}".format(endpoint, json_resp['results'][0]['uuid'])
-                print("Reporter exits: [URL:{0}]".format(url))
-                post_data = json.dumps(data2)
-                resp = post_request(post_data, url=url)
-                # print(json_resp['results'])
-            else:
-                print("Reporter missing in RapidPro")
-                post_data = json.dumps(data)
-                resp = post_request(post_data)
+        role_filter = lambda x: x != ''
+        try:
+            if phone:
+                data = {
+                    "name": r['name'],
+                    "urns": ["tel:" + phone],
+                    "email": r['email'],
+                    "groups": list(filter(role_filter,r['role'].split(','))),
+                    "fields": fields
+                }
+                data2 = {
+                    "name": r['name'],
+                    "email": r['email'],
+                    "groups": list(filter(role_filter,r['role'].split(','))),
+                    "fields": fields
+                }
+                # print("======>", list(filter(role_filter,r['role'].split(','))))
+                # Let's try getting the contact
+                get_url = "{0}urn={1}".format(endpoint, "tel:" + phone)
+                # print(get_url)
+                resp = get_request(get_url)
+                json_resp = resp.json()
+                if json_resp['results']:
+                    url = "{0}uuid={1}".format(endpoint, json_resp['results'][0]['uuid'])
+                    print("Reporter exits: [URL:{0}]".format(url))
+                    post_data = json.dumps(data2)
+                    resp = post_request(post_data, url=url)
+                    # print(json_resp['results'])
+                else:
+                    print("Reporter missing in RapidPro")
+                    post_data = json.dumps(data)
+                    resp = post_request(post_data)
 
-            print("Response:=>", resp.json())
-            response_dict = resp.json()
-            if 'uuid' in response_dict:
-                contact_uuid = response_dict["uuid"]
-                cur.execute("UPDATE reporters SET uuid = %s WHERE id=%s", [contact_uuid, r["id"]])
-                conn.commit()
-                print("Added/Updated: ", r['name'], phone)
+                print("Response:=>", resp.json())
+                response_dict = resp.json()
+                if 'uuid' in response_dict:
+                    contact_uuid = response_dict["uuid"]
+                    cur.execute("UPDATE reporters SET uuid = %s WHERE id=%s", [contact_uuid, r["id"]])
+                    conn.commit()
+                    print("Added/Updated: ", r['name'], phone)
+                else:
+                    pass
             else:
-                pass
+                print(f"Invalid Phone: {r['telephone']}")
 
-            # print resp.text
+                # print resp.text
 
-        if alt_phone:
-            existing_uuid = r["uuid2"]
-            endpoint = config["default_api_uri"]
-            if '?' not in endpoint:
-                endpoint += "?"
-            alt_phone = format_msisdn(alt_phone)
-            post_data = json.dumps(data)
-            data = {
-                "name": r['name'],
-                "urns": ["tel:" + phone],
-                "email": r['email'],
-                "groups": r['role'].split(','),
-                "fields": fields
-            }
-            data2 = {
-                "name": r['name'],
-                "email": r['email'],
-                "groups": r['role'].split(','),
-                "fields": fields
-            }
-            # Let's try getting the contact
-            get_url = "{0}urn={1}".format(endpoint, "tel:" + alt_phone)
-            print(get_url)
-            resp = get_request(get_url)
-            json_resp = resp.json()
-            if json_resp['results']:
-                url = "{0}uuid={1}".format(endpoint, json_resp['results'][0]['uuid'])
-                print("Reporter exits: [URL:{0}]".format(url))
-                post_data = json.dumps(data2)
-                resp = post_request(post_data, url=url)
-                # print(json_resp['results'])
-            else:
-                print("Reporter missing in RapidPro")
-                post_data = json.dumps(data)
-                resp = post_request(post_data)
+            if alt_phone:
+                existing_uuid = r["uuid2"]
+                endpoint = config["default_api_uri"]
+                if '?' not in endpoint:
+                    endpoint += "?"
+                alt_phone = format_msisdn(alt_phone)
+                # post_data = json.dumps(data)
+                data = {
+                    "name": r['name'],
+                    "urns": [f"tel:{phone}"],
+                    "email": r['email'],
+                    "groups": list(filter(role_filter,r['role'].split(','))),
+                    "fields": fields
+                }
+                data2 = {
+                    "name": r['name'],
+                    "email": r['email'],
+                    "groups": list(filter(role_filter,r['role'].split(','))),
+                    "fields": fields
+                }
+                # Let's try getting the contact
+                get_url = "{0}urn={1}".format(endpoint, f"tel:{alt_phone}")
+                print(get_url)
+                resp = get_request(get_url)
+                json_resp = resp.json()
+                if json_resp['results']:
+                    url = "{0}uuid={1}".format(endpoint, json_resp['results'][0]['uuid'])
+                    print("Reporter exits: [URL:{0}]".format(url))
+                    post_data = json.dumps(data2)
+                    resp = post_request(post_data, url=url)
+                    # print(json_resp['results'])
+                else:
+                    print("Reporter missing in RapidPro")
+                    post_data = json.dumps(data)
+                    resp = post_request(post_data)
 
-            print "=>", resp.json()
-            response_dict = resp.json()
-            if 'uuid' in response_dict:
-                contact_uuid = response_dict["uuid"]
-                cur.execute("UPDATE reporters SET uuid2 = %s WHERE id=%s", [contact_uuid, r["id"]])
-                conn.commit()
-                print("Added/Updated:", response_dict)
-            else:
-                pass
-            print resp.text
-            time.sleep(0.3)
+                print("=>", resp.json())
+                response_dict = resp.json()
+                if 'uuid' in response_dict:
+                    contact_uuid = response_dict["uuid"]
+                    cur.execute("UPDATE reporters SET uuid2 = %s WHERE id=%s", [contact_uuid, r["id"]])
+                    conn.commit()
+                    print("Added/Updated:", response_dict)
+                else:
+                    pass
+
+                print(resp.text)
+        except Exception as e:
+            print("XXXXX", e)
+            continue
+        time.sleep(0.3)
 conn.close()
